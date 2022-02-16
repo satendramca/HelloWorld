16c85b5589eb7b2a085e98bef1998fc9b8619695
satendramca <99817442+satendramca@users.noreply.github.com>
2022-02-16 20:21:23 +0530

    Add files via upload

---
+++ HTML/javascript/javascript/javascript.js
@@ -0,0 +1,14 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Satendra.";
+}
+//single line comment
+/*multiline 
+Comment*/
+
+var a;
+a=30;
+function myFunction(){
+  let x=2;
+  console.log(x)
+  console.log(a);
+}
